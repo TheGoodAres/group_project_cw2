7c69433e3316116ee0155cc3bd7a5ff447b11958
TheGoodAres <oprearobert0202@gmail.com>
2022-07-02 10:33:25 +0100

    modified Register_GUI: -repositioned components

--- src/main/java/com/mycompany/group_project_cw2/Register_GUI.java
+++ src/main/java/com/mycompany/group_project_cw2/Register_GUI.java
@@ -6,24 +6,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Register_GUI extends JFrame implements ActionListener {
+        //container
         Container container = getContentPane();
+        //Labels
         JLabel registerTitle = new JLabel("REGISTER");
         JLabel email = new JLabel("Email");
         JLabel password = new JLabel("Password");
-
         JLabel confirmPassword = new JLabel("Confirm password");
-        JTextField emailTextField = new JTextField();
-        JPasswordField passwordField = new JPasswordField();
-
-        JPasswordField confirmPasswordField = new JPasswordField();
-        JButton registerButton = new JButton("Register");
-        JButton loginButton = new JButton("LOGIN HERE");
-
-        JLabel firstName = new JLabel("First name");
-        JLabel lastName = new JLabel("Last name");
-
+        //Text fields
         JTextField firstNameField = new JTextField();
         JTextField lastNameField = new JTextField();
+        JTextField emailTextField = new JTextField();
+        JLabel firstName = new JLabel("First name");
+        JLabel lastName = new JLabel("Last name");
+        //Password field
+        JPasswordField passwordField = new JPasswordField();
+        JPasswordField confirmPasswordField = new JPasswordField();
+        //Buttons
+        JButton registerButton = new JButton("Register");
+        JButton loginButton = new JButton("LOGIN HERE");
         Register_GUI() {
             setLayout();
             setComponentsLocation();
@@ -35,22 +36,22 @@ public class Register_GUI extends JFrame implements ActionListener {
         }
         public void setComponentsLocation() {
             //title
-            registerTitle.setBounds(180, 20, 200, 100);
+            registerTitle.setBounds(180, 10, 200, 80);
            //labels
-            email.setBounds(70,180,150,40);
-            password.setBounds(70,220,150,40);
-            confirmPassword.setBounds(70, 260, 150, 40);
             firstName.setBounds(70, 95, 150, 40);
-            lastName.setBounds(70, 135, 150, 40);
+            lastName.setBounds(70, 160, 150, 40);
+            email.setBounds(70,225,150,40);
+            password.setBounds(70,290,150,40);
+            confirmPassword.setBounds(70, 355, 150, 40);
             //text fields
-            emailTextField.setBounds(190,180,150,30);
-            passwordField.setBounds(190,220,150,30);
-            confirmPasswordField.setBounds(190,260,150,30);
             firstNameField.setBounds(190, 100,150, 30);
-            lastNameField.setBounds(190, 140, 150, 30);
+            lastNameField.setBounds(190, 165, 150, 30);
+            emailTextField.setBounds(190,230,150,30);
+            passwordField.setBounds(190,295,150,30);
+            confirmPasswordField.setBounds(190,360,150,30);
             //buttons
-            loginButton.setBounds(85,330,100,30);
-            registerButton.setBounds(235,330,100,30);
+            loginButton.setBounds(85,430,100,30);
+            registerButton.setBounds(235,430,100,30);
 
 
         }
